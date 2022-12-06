e14f79250a3d71e5d91858c906d084b9dcf5768a
Dmitry <bog22232@ya.ru>
2022-12-06 20:09:45 +0300

    fix block ui raycast

--- Assets/Script/InputChecker/Base/IObjectClickChecker.cs
+++ Assets/Script/InputChecker/Base/IObjectClickChecker.cs
@@ -4,6 +4,7 @@ namespace Script.InputChecker.Base
 {
 public interface IObjectClickChecker
 {
+    bool IsBlockedByUI { get; }
     event Action ObjectClicked;
     void OnObjectClicked();
     void Activate();
--- Assets/Script/InputChecker/MouseKeyboard/MouseClickChecker.cs
+++ Assets/Script/InputChecker/MouseKeyboard/MouseClickChecker.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using Script.InputChecker.Base;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 namespace Script.InputChecker.MouseKeyboard
 {
 public class MouseClickChecker : IObjectClickChecker
 {
+    public bool IsBlockedByUI { get; }
     public event Action ObjectClicked;
 
     private readonly Camera _mainCamera;
     private readonly InputAction _mouseClickInputAction;
     private readonly Collider _trackCollider;
     
-    public MouseClickChecker(Camera mainCamera, InputAction mouseClickInputAction, Collider trackCollider)
+    public MouseClickChecker(Camera mainCamera, InputAction mouseClickInputAction, Collider trackCollider, bool isU = true)
     {
+        IsBlockedByUI = isU;
         _trackCollider = trackCollider;
         _mainCamera = mainCamera;
         _mouseClickInputAction = mouseClickInputAction;
@@ -37,8 +40,13 @@ public class MouseClickChecker : IObjectClickChecker
 
     private void OnMouseInputPerformed(InputAction.CallbackContext context)
     {
-        var screenPointToRay = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (IsBlocked())
+        {
+            return;
+        }
         
+        var screenPointToRay = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
         if (Physics.Raycast(ray: screenPointToRay, hitInfo: out var hit) && hit.collider)
         {
             if (hit.collider == _trackCollider)
@@ -46,6 +54,27 @@ public class MouseClickChecker : IObjectClickChecker
                 OnObjectClicked();
             }
         }
+    }
+
+    private bool IsBlocked()
+    {
+        var isBlocked = IsUIBlock();
+
+        return isBlocked;
+    }
+
+    private bool IsUIBlock()
+    {
+        if (IsBlockedByUI)
+        {
+            var isPointerOverGameObject = EventSystem.current.IsPointerOverGameObject();
+            if (isPointerOverGameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 }
